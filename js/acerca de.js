1e0e75e9922c29a5baf161e1ce6719b2ceb2fe62
ADBERR1 <130815926+ADBERR1@users.noreply.github.com>
2024-07-16 10:27:24 -0600

    Delete js directory

--- js/acerca de.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("contactButton").addEventListener("click", function() {
-    alert("¡Gracias por contactarnos! Te responderemos pronto.");
-});
--- js/contacto.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe
-
-    // Aquí puedes agregar el código para procesar el formulario, como enviar datos a un servidor o mostrar un mensaje de confirmación.
-    // Por ejemplo, puedes utilizar JavaScript para validar los campos antes de enviarlos.
-
-    alert("¡Gracias por su mensaje! Le responderemos pronto.");
-    document.getElementById("contactForm").reset(); // Limpiar el formulario
-});
